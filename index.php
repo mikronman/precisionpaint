20f3edad9f7163547e1878e8b85ffe38158b4f68
mikronman <mkleczkajr@gmail.com>
2023-07-09 12:46:57 -0400

    fixed minor path issue on index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    require __DIR__ . '/../vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
     if (file_exists(__DIR__ . '/../.env')) {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
